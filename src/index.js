d53c8b6d42c71442f8a2d056ba737bceb419ef30
josejavierpaez <josejavierpaez@hotmail.com>
2021-05-23 02:15:17 -0400

    ✨ feat(logger): add global logger

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const app = require('./app');
+const log = require('./helpers/logger');
 
 const PORT = process.env.PORT || 3000;
 
 async function main() {
   await app.listen(PORT);
-  console.log(`server on port ${PORT}`);
+  log.info(`server on port ${PORT}`);
 }
 
 main();
