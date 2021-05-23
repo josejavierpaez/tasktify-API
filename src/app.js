468f78a3249b919b23c42b8840eff7be8d7e5573
josejavierpaez <josejavierpaez@hotmail.com>
2021-05-23 19:33:00 -0400

    ✨ feat(swagger): add swagger

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 require('dotenv').config();
 const express = require('express');
+const swagger = require('swagger-ui-express');
+const swaggerConfig = require('./config/swagger');
 const { errorHandler, morgan, healthCheck } = require('./middleware');
 
 const app = express();
@@ -13,7 +15,8 @@ app.use(
   }),
 );
 
-app.use(`${'/healthcheck'} || ${'/'}`, healthCheck);
+app.use('/api/v1/doc', swagger.serve, swagger.setup(swaggerConfig));
+app.use(`${'/api/v1/health'} || ${'/'}`, healthCheck);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(errorHandler.errorProductionResponse);
